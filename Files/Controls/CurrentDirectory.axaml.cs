dac96929786a8b05c9c74f8bf73cbf8a596661a1
Eris M <erismathison@gmail.com>
2025-05-16 19:20:20 +1000

    Added CurrentDirectory.axaml and UI changes.

---
+++ Files/Controls/CurrentDirectory.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Files.Controls;
+
+public partial class CurrentDirectory : UserControl
+{
+    public CurrentDirectory()
+    {
+        InitializeComponent();
+    }
+}
--- Files/ViewModels/MainWindowViewModel.cs
+++ Files/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -11,9 +12,10 @@ namespace Files.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
- 
     [ObservableProperty]
     private ObservableCollection<DirectoryItemViewModel> _items = [];
+    [ObservableProperty]
+    private int _columns = 10;
     private Explorer _explorer;
 
     public MainWindowViewModel()
@@ -22,13 +24,22 @@ public partial class MainWindowViewModel : ViewModelBase
         RefreshItems();
     }
 
-    public int Columns { get; set; } = 10;
+    public string CurrentDirectory
+    {
+        get => _explorer.CurrentDirectory;
+        set
+        {
+            SetProperty(_explorer.CurrentDirectory, value, _explorer, (e, s) => e.CurrentDirectory = s);
+            RefreshItems();
+        }
+    }
 
     private void SelectItem(DirectoryItem item)
     {
         if (item.Kind == DirectoryItemKind.Directory)
         {
             _explorer.EnterSubdirectory(item.Name);
+            OnPropertyChanged(nameof(CurrentDirectory));
             RefreshItems();
             return;
         }
